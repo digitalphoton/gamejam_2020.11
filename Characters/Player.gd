732723d4d5a0153035fcf4b015515b6f8f17f606
João V. S. Anacleto <22249746+digitalphoton@users.noreply.github.com>
2020-11-21 18:46:26 -0300

    mudancas anteriores magesu

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 
 #Variáveis exportáveis
 #Stats
+export var active = false
 export var strength = 500
+export var camlimits = {"Left":-10000000,"Top":-10000000,"Right":10000000,"Bottom":10000000}
 
 #Respawn Data
 export(String,FILE,"*.tscn") var spawn_scene_path
@@ -13,12 +15,23 @@ export var spawn_coordinates = Vector2()
 var player_input = {}
 
 #Nodes
-onready var controller = get_node("Controller")
+onready var controller 	= get_node("Controller")
+onready var camera 		= get_node("Camera2D")
 
 func _ready():
-	pass
+	camera.limit_left = camlimits.Left
+	camera.limit_top = camlimits.Top
+	camera.limit_right = camlimits.Right
+	camera.limit_bottom = camlimits.Bottom
 
 func _process(delta):
+	#Muda o current da camera de acordo com a var active
+	#Ou seja, quando o active é true a câmera desse personagem é usada quando o active é false a camera desse personagem não é usada
+	if camera.current != active:
+		camera.current = active
+	if controller.input_enabled != active:
+		controller.input_enabled = active
+	
 	#Player controls. Teclas podem ser encontradas em Project>>Project Settings>>Input Map
 	player_input.right = Input.is_action_pressed("Right")
 	player_input.left = Input.is_action_pressed("Left")
