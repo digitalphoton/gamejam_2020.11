5034ed50dcdfdd0049bcad5130b48fad228e1f3f
Pedestrek <63029944+Pedestrek@users.noreply.github.com>
2020-11-22 16:00:20 -0300

    Better door

--- StaticBody2D.gd
+++ StaticBody2D.gd
@@ -6,18 +6,28 @@ extends RigidBody2D
 # var b = "text"
 var y = 0
 export var y_dif = 512
-export var vel = 4
+export var vel = 8
 var doorOn = false
+var rotx
+var roty
 
+func _process(_delta):
+	
+	rotx = cos(-self.transform.get_rotation() - PI/2)
+	roty = sin(-self.transform.get_rotation() - PI/2)
+	print(str(rotx) + "   " + str(roty))
 
 func _on_Middle_button_Trigger(buttonOn):
 	
 	doorOn = buttonOn
 
 func _physics_process(_delta):
+	
 	if doorOn and y < y_dif:
-		self.position += Vector2(0, -vel)
-		y += vel
+		self.position += Vector2(-rotx, roty)
+		y += 1
 	if not doorOn and y > 0:
-		self.position += Vector2(0, vel)
-		y += -vel
+		self.position += Vector2(rotx, -roty)
+		y += -1
+	
+	
