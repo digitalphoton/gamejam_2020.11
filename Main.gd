f92f31395e12b88f2bfed3203e84f6002cd28181
Magesu <44463720+Magesu@users.noreply.github.com>
2020-11-22 19:18:01 -0300

    Revert "Coisas de música e arrumei o troço da força"
    This reverts commit 0b1d890df11438aff6a0e2ec4e3681cbf7abd2fa.

--- Main.gd
+++ Main.gd
@@ -1,42 +1,14 @@
 #Main.gc
 extends Node
 
-#Nodes
-onready var BGM = get_node("BGM")
-
 #Variáveis
 var current_scene
-
-#Variáveis exportáveis
-#BGMs
-export var BGMs = {"StartMenu":"res://Music/menu.ogg"}
 
 func change_scene(scene,new_scene_path):
 	call_deferred("_deferred_change_scene",scene,new_scene_path)
 
 func _deferred_change_scene(scene,new_scene_path):
-	if scene != null:
-		scene.free()
+	scene.free()
 	var l = load(new_scene_path)
 	current_scene = l.instance()
 	self.add_child(current_scene)
-
-func _on_Start_Menu_menu_bgm():
-	if BGM.stream != load(BGMs.StartMenu):
-		BGM.stream = load(BGMs.StartMenu)
-		BGM.play()
-
-func _on_Map_Select_menu_bgm():
-	if BGM.stream != load(BGMs.StartMenu):
-		BGM.stream = load(BGMs.StartMenu)
-		BGM.play()
-
-func _on_DebugMap_map_bgm():
-	if BGM.stream != load(BGMs.LevelMusic):
-		BGM.stream = load(BGMs.LevelMusic)
-		BGM.play()
-
-func _on_NewMapIHope_map_bgm():
-	if BGM.stream != load(BGMs.LevelMusic):
-		BGM.stream = load(BGMs.LevelMusic)
-		BGM.play()
--- Menus/Start Menu.gd
+++ Menus/Start Menu.gd
@@ -4,9 +4,6 @@ extends Control
 #Nodes
 onready var main = get_parent()
 
-#Sinais
-signal menu_bgm
-
 func _ready():
 	pass
 
@@ -15,8 +12,3 @@ func _on_START_pressed():
 
 func _on_QUIT_pressed():
 	get_tree().quit()
-
-
-func _on_Main_ready():
-	connect("menu_bgm",main,"_on_Start_Menu_menu_bgm")
-	emit_signal("menu_bgm")
