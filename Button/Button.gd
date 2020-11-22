0a9a49fd263de990dc0a73977a5c23ba5f152bd1
Pedestrek <63029944+Pedestrek@users.noreply.github.com>
2020-11-22 00:30:42 -0300

    Door i think

--- Button/Button.gd
+++ Button/Button.gd
@@ -10,17 +10,24 @@ var is_colliding
 var colliding_obj
 export var vel = 2
 onready var A2D = get_node("Area2D")
+<<<<<<< Updated upstream:Button/Button.gd
 <<<<<<< Updated upstream
 var doorOn
 =======
 >>>>>>> Stashed changes
 
 signal Button_Trigger
+=======
+var buttonOn
+
+signal button_Trigger
+>>>>>>> Stashed changes:Button/Middle.gd
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_contact_monitor(true)
 	set_max_contacts_reported(2048)
+<<<<<<< Updated upstream:Button/Button.gd
 <<<<<<< Updated upstream
 	
 func _process(_delta):
@@ -34,6 +41,16 @@ func _process(_delta):
 
 
 >>>>>>> Stashed changes
+=======
+	
+func _process(_delta):
+	if not buttonOn and y > y_dif/2:
+		buttonOn = true
+		emit_signal("button_Trigger", buttonOn)
+	if buttonOn and y < y_dif/2:
+		buttonOn = false
+		emit_signal("button_Trigger", buttonOn)
+>>>>>>> Stashed changes:Button/Middle.gd
 
 func collision():
 	is_colliding = false
@@ -41,16 +58,20 @@ func collision():
 	for body in A2D.get_overlapping_bodies():
 		print(body)
 		#Detecta se o objeto que colidiu pode ser movido
-		if body.is_in_group("MovableObjects"):
+		if body.is_in_group("ButtonTriggers"):
 			colliding_obj.append(body)
 			is_colliding = true
 	
 
+<<<<<<< Updated upstream:Button/Button.gd
 <<<<<<< Updated upstream
 func _physics_process(_delta):
 =======
 func _physics_process(delta):
 >>>>>>> Stashed changes
+=======
+func _physics_process(_delta):
+>>>>>>> Stashed changes:Button/Middle.gd
 	collision()
 	if is_colliding and y < y_dif:
 		self.position += Vector2(0, vel)
