258fd27a33e9ba533c04e809910dadb8d5f2f1e8
Magesu <44463720+Magesu@users.noreply.github.com>
2020-11-22 01:53:46 -0300

    Menu level select adicionado
    Pra adicionar um nível é só adicionar o caminho pro arquivo .tscn no array de maps na cena level select, adicionar um botão, conectar o sinal do button pressed no script do level select e tacar um main.change_scene(maps[número de botões - 1]).
    Also fonte de minecraft adicionada

---
+++ Menus/Map_Select.gd
@@ -0,0 +1,19 @@
+#Map_Select.gd
+tool
+extends Control
+
+#Nodes
+onready var button_container = get_node("MarginContainer/VBoxContainer/VBoxContainer")
+onready var main = self.get_parent()
+
+#Variáveis exportáveis
+export var maps = []
+
+#Variáveis
+var n_maps
+
+func _ready():
+	n_maps = maps.size()
+
+func _on_DebugMap_pressed():
+	main.change_scene(self,maps[0])
